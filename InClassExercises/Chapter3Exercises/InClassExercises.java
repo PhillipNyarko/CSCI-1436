7cf200f5b7f07e1f6a6b54816f36176027a381f7
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-09-22 13:54:12 -0500

    added switch statement method to in class exercises

--- InClassExercises/Chapter3Exercises/InClassExercises.java
+++ InClassExercises/Chapter3Exercises/InClassExercises.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class InClassExercises {
 	public static void main(String args[]) {
-		randIntToSqr();
+		rightTriangleStars();
 	}
 	
 	public static void simpleIfDemo() {
@@ -52,4 +52,30 @@ public class InClassExercises {
 		}
 		input.close();
 	}
+	
+	public static int switchStatement(int number){
+		switch (day){				
+			case 1:
+				System.out.println("Monday");
+				break;
+			case 2:
+			       	System.out.println("Tueday");
+				break;
+			case 3:
+			       	System.out.println("Wednesday");
+				break;
+		 	case 4:
+			       	System.out.println("Thursday");
+				break;
+			case 5:
+			       	System.out.println("Friday");
+				break;
+			case 6:
+			       	System.out.println("Saturday");
+				break;
+			case 7: System.out.println("Sunday");
+				break;
+			default:
+				System.out.println("Please enter a valid number from 1-7")
+	{
 }
