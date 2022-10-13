ea6f4ba88c780b2f3b67f0d12ceccadf754ecc76
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-10-12 19:40:44 -0500

    final commit for lab 8

--- Labs/Lab8_Phillip_Nyarko/Lab8.java
+++ Labs/Lab8_Phillip_Nyarko/Lab8.java
@@ -4,7 +4,7 @@ public class Lab8 { // main class
 	public static void main(String args[]) { // main method
 		
 		Scanner input = new Scanner(System.in); // declare and initialize scanner
-		System.out.print("Enter the number of rows in the conversion table: "); // get user input
+		System.out.print("Enter the number of rows in the conversion table: "); // prompt the user for input
 		int numberOfRows = input.nextInt();  // store the user input as the number of rows to be displayed in the table;
 		
 		// use three separate print statements to print each line of the header
@@ -19,14 +19,14 @@ public class Lab8 { // main class
 		/* Only the odd number calculations should be printed, so the number of rows value is multiplied by two and the
 		 * "i" incrementing variable is increased by 2 each loop. This prints the right amount of rows while also
 		 * not printing even values*/
-		do{
+                while (i < numberOfRows*2){ // only execute loop while the loop variable is less then double the number of rows
 			/* use string formatting and the decimal percision formatting syntax to left justify each element
 			 * that is on the left side of its column, right justify each element on the right side of its column, 
 			 * and determine how much decimal precision will be alloted for each value. Then create a new line.*/
 			System.out.printf("%-10.0f  %9.1f | %-10.0f %11.2f\n", (double) i, i * 2.2, pounds, pounds/2.2);
 			pounds += 5; // increment the number of pounds to be calculated and printed by 5
-			i += 2; // increment the loop control variabl by 2
-		}while (i < numberOfRows*2); 
+			i += 2; // increment the loop control variable by 2
+		}
 		System.out.println("=============================================="); // print the bottom barrier for the table
 		input.close(); // close the scanner
 	}
