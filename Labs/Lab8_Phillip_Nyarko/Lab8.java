c1b3bc2cadd2232f3ff79a0c079d9d4165135030
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-10-12 19:20:39 -0500

    for loop changed to do-while loop

--- Labs/Lab8_Phillip_Nyarko/Lab8.java
+++ Labs/Lab8_Phillip_Nyarko/Lab8.java
@@ -3,21 +3,31 @@ import java.util.Scanner; // import Scanner class
 public class Lab8 { // main class
 	public static void main(String args[]) { // main method
 		
-		Scanner input = new Scanner(System.in); // declare  and initialize scanner
-		System.out.print("Enter the number of rows in the conversion table:  "); // get user input
-		int numberOfRows = input.nextInt();  // store the user inputs as the number of rows to be displayed in the table;
+		Scanner input = new Scanner(System.in); // declare and initialize scanner
+		System.out.print("Enter the number of rows in the conversion table: "); // get user input
+		int numberOfRows = input.nextInt();  // store the user input as the number of rows to be displayed in the table;
 		
 		// use three separate print statements to print each line of the header
-		System.out.println("\n=============================================="); // the "\n" creates a new line before creating the table
-		System.out.println("kilograms     pounds  |  pounds      kilograms"); // print the units of the header in the correct spot
-		System.out.println("----------------------------------------------"); // the "\n" creates a new line before creating the table
+		// first print statment adds a new line before printing the table
+		System.out.println("\n=============================================="); 
+		System.out.println("kilograms     pounds  |  pounds      kilograms"); 
+		System.out.println("----------------------------------------------");
 
-		double initialPounds = 20;
-		for(int i = 1; i < numberOfRows*3; i+=2) {
-			System.out.printf("%-10.0f  %9.1f | %-10.0f %11.2f\n", (double) i, i * 2.2, initialPounds, initialPounds/2.2);
-			initialPounds += 5;
-		}
+		double pounds = 20; // declare a variable to store the inital amount of pounds used in the calculation
+		int i = 1; // declare loop control variable	    
+		
+		/* Only the odd number calculations should be printed, so the number of rows value is multiplied by two and the
+		 * "i" incrementing variable is increased by 2 each loop. This prints the right amount of rows while also
+		 * not printing even values*/
+		do{
+			/* use string formatting and the decimal percision formatting syntax to left justify each element
+			 * that is on the left side of its column, right justify each element on the right side of its column, 
+			 * and determine how much decimal precision will be alloted for each value. Then create a new line.*/
+			System.out.printf("%-10.0f  %9.1f | %-10.0f %11.2f\n", (double) i, i * 2.2, pounds, pounds/2.2);
+			pounds += 5; // increment the number of pounds to be calculated and printed by 5
+			i += 2; // increment the loop control variabl by 2
+		}while (i < numberOfRows*2); 
 		System.out.println("=============================================="); // print the bottom barrier for the table
-		input.close();
+		input.close(); // close the scanner
 	}
 }
