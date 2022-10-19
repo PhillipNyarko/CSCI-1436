19d829ec29e1c2be09d6faabb3940131a29b90b7
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-10-19 15:03:21 -0500

    Lab9

---
+++ Labs/Lab9_Phillip_Nyarko/Lab9.java
@@ -0,0 +1,22 @@
+class Lab9{
+
+	public static void main(String args[]){
+		for(int i = 6; i > 0; i--){
+			for(int j = 0; j < i; j++){
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+		for(int i = 0; i < 6; i++){
+			for(int j = 1; j < 6-i; j++){
+			       System.out.print(" ");	       
+			}
+			for(int k = 0; k <= i; k++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}	
+		
+	}
+}
