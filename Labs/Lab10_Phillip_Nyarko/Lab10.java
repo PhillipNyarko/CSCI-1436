d3f7c456f53cf2b7362dcbcd6099afce67b690b1
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-10-20 18:38:46 -0500

    added while loop to try except

--- Labs/Lab10_Phillip_Nyarko/Lab10.java
+++ Labs/Lab10_Phillip_Nyarko/Lab10.java
@@ -5,28 +5,33 @@ class Lab10{
 	public static void main(String args[]){
 		String[] computerChoicesArray = {"rock", "paper", "scissors"};
 		String computerChoice =  computerChoicesArray[(int)(Math.random() * 3)];
-		System.out.println(computerChoice);
-		String gameOutcome;
-		try{
+		String gameOutcome = "";
 		Scanner input = new Scanner(System.in);
-		System.out.println("Welcome to Rock, Paper, Scissors!");
-		TimeUnit.SECONDS.sleep(2);
-		System.out.print("What is your move? (Enter \"Rock\", \"Paper\", or \"Scissors\" to play or enter \"quit\" to quit:) ");
-		System.out.print("Your Move: ");
 		String userGuess = input.nextLine().toLowerCase();
-		if("quit".equals(userGuess)){
-			System.out.println("Thanks for playing Rock, Paper, Scissors!");
-		}else if(computerChoice.equals(userGuess)){
-			gameOutcome = "It's a tie!";
-		}else if (userGuess.equals("rock") && computerChoice.equals("scissors") || userGuess.equals("paper") && computerChoice.equals("rock") || userGuess.equals("scissors") && computerChoice.equals("paper")){
-			gameOutcome = "You Won!";
-		}else{
-			gameOutcome = "You Lost!";
-		}
-		TimeUnit.SECONDS.sleep(1);
-		System.out.println("Opponent Move: " +  computerChoice);
-		TimeUnit.SECONDS.sleep(1);
-		System.out.println(gameOutcome);
+		System.out.println("Welcome to Rock, Paper, Scissors!");
+
+		try{
+		TimeUnit.SECONDS.sleep(2);
+		
+			while(userGuess != "quit"){
+				System.out.print("What is your move? (Enter \"Rock\", \"Paper\", or \"Scissors\" to play or enter \"quit\" to quit:) ");
+				System.out.print("Your Move: ");
+				userGuess = input.nextLine().toLowerCase();
+				if("quit".equals(userGuess)){
+					System.out.println("Thanks for playing Rock, Paper, Scissors!");
+					break;
+				}else if(computerChoice.equals(userGuess)){
+					gameOutcome = "It's a tie!";
+				}else if (userGuess.equals("rock") && computerChoice.equals("scissors") || userGuess.equals("paper") && computerChoice.equals("rock") || userGuess.equals("scissors") && computerChoice.equals("paper")){
+					gameOutcome = "You Won!";
+				}else{
+					gameOutcome = "You Lost!";
+				}
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println("Opponent Move: " +  computerChoice);
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println(gameOutcome);
+			}
 		} catch (InterruptedException e){
 			Thread.currentThread().interrupt();
 			System.out.println("Thread Interruption Exception Reached");
