d0598f5db3095811f068eac3ddefb227015ebb42
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-11-17 08:13:33 -0600

    temp.java

--- temp.java
+++ temp.java
@@ -1,15 +1,18 @@
-import java.util.Arrays;
+class question1 { public static void main(String args[]) {
+    int matrix[][] = {{1, 0, 2, 2}, 
+                      {0, 0, 1, 3},
+                      {0, 1, 1, 2}};
 
-public class temp {
-  public static void main(String args[]) {
-    int matrix[][] = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}, {16, 17, 18, 19}};
     int total = 0;
+    int rows = matrix.length;
+    int cols = matrix[0].length;
 
-    for(int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[0].length; j++) {
-        total += matrix[i][j];
+    for(int j = 0; j < cols; j++){  
+      total = 0;  
+      for(int k = 0; k < rows; k++){  
+        total += matrix[k][j];  
       }
+      System.out.print(total + " ");
     }
-    System.out.println(total);
   }
 }
