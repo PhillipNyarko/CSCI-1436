e8b39b11fc384afe8372dcb15423f38fc30b5437
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2022-12-02 23:51:39 -0600

    comments added

--- Labs/Lab15_Phillip_Nyarko/TestRandomTriangle.java
+++ Labs/Lab15_Phillip_Nyarko/TestRandomTriangle.java
@@ -1,21 +1,23 @@
 class TestRandomTriangle{
   public static void main(String args[]){
 
+    // create an array with random character values
     char[] symbols = new char[] {'!', '@', '#', '$', '%', '+', 'Z', '?', '<', '/', '|'};
 
-    Triangle t = new Triangle();
+    Triangle t = new Triangle(); // create a triangle object
 
+    // in the while loop, draw the triangle, pause for 1 second, then randomly change all of its variables
     while(true){
       t.drawTriangle();
       sleep(1);
       clearScreen();
       t.setSymbol(symbols[(int)(Math.random()*symbols.length - 1)]);
       t.setRows((int)(Math.random()*10) + 3);
-      int[] position = new int[] {(int)(Math.random()*20), (int)(Math.random()*20)};
+      int[] position = new int[] {(int)(Math.random()*20), (int)(Math.random()*20)}; // array with 2 random ints
       t.setPosition(position);
     }
   }
-
+  // method to sleep 
   public static void sleep(int seconds){
     try {
       Thread.sleep(seconds * 1000);
@@ -23,7 +25,7 @@ class TestRandomTriangle{
       e.printStackTrace();
     }
   }
-
+ // method to clear screen 
   public static void clearScreen(){
     System.out.print("\033[H\033[2J");  
     System.out.flush();
--- Labs/Lab15_Phillip_Nyarko/Triangle.java
+++ Labs/Lab15_Phillip_Nyarko/Triangle.java
@@ -1,8 +1,14 @@
-public class Triangle{
+public class Triangle{ // create the triangle class 
+  /* based on the image you have in the lab example the
+  triangles are randomly generated which means there is no need
+  for an explicit constructor*/
+
+  // init class variables for the row size, symbol value, and position(x,y) values
   int rows = 3;
   char symbol = '*';
   int[] position = new int[]{20, 20};
 
+  // create get and set methods for all three variables
   public int getRows(int rows){
     return rows;
   }
@@ -27,12 +33,15 @@ public class Triangle{
     symbol = newSymbol;
   }
 
+  // create a method to draw the acute triangle
   public void drawTriangle(){
     int k = 0;
+      // call the get position method and print extra spaces based on the function's first value
       for (int i = 0; i < getPosition(position)[1]; i++) {
         System.out.println();
       }
       for (int i = 1; i <= rows; ++i, k = 0) { // create a nested loop that increases the amount of spaces printed as the row number increases
+        // call the get position method and print extra spaces based on the function's second value
         for (int j = 0; j < getPosition(position)[0]; j++) {
          System.out.print(" ");
         } 
